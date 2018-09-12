a2166e0b33819371d548800f5f74152ff1180e05
haomom <gchaomom@gmail.com>
2018-09-12 17:32:33 +0100

    12092018

--- src/Mizuho.London.FinanceLedgerPosting.WebApi/App_Start/UnityConfig.cs
+++ src/Mizuho.London.FinanceLedgerPosting.WebApi/App_Start/UnityConfig.cs
@@ -1,33 +1,26 @@
+using Microsoft.Practices.Unity;
 using Mizuho.London.FinanceLedgerPosting.Data.Entities;
 using Mizuho.London.FinanceLedgerPosting.Repository.Infrastructure;
 using Mizuho.London.FinanceLedgerPosting.Repository.Interfaces;
 using Mizuho.London.FinanceLedgerPosting.Repository.Repositories;
 using System.Web.Http;
-using Unity;
-using Unity.Lifetime;
 using Unity.WebApi;
 
 namespace Mizuho.London.FinanceLedgerPosting.WebApi
 {
-    /// <summary>
-    /// This class is for mapping all components for IOC
-    /// </summary>
     public static class UnityConfig
     {
-        /// <summary>
-        /// Register all components for IOC
-        /// </summary>
         public static void RegisterComponents()
         {
-			var container = BuildUnityContainer();
-           
+            var container = BuildUnityContainer();
+            
             GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
         }
 
         private static IUnityContainer BuildUnityContainer()
         {
             var container = new UnityContainer();
-            
+
             container.RegisterType<IUnitOfWork, FinanceLedgerPostingDbContext>(new HierarchicalLifetimeManager());
 
             #region Model
